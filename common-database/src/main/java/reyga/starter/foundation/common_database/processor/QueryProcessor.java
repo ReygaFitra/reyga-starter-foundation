78202a472501151a9a03384df38abd9225cde708
ReygaFitra <Reyga.Fitra.Ramadhan@bni.co.id>
2025-09-01 10:22:42 +0700

    [dev1.0] - Implemnting Abstract QueryProcessor

--- common-database/src/main/java/reyga/starter/foundation/common_database/processor/QueryProcessor.java
+++ common-database/src/main/java/reyga/starter/foundation/common_database/processor/QueryProcessor.java
@@ -1,10 +1,125 @@
 package reyga.starter.foundation.common_database.processor;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import reyga.starter.foundation.common.logging.BaseLogging;
+import reyga.starter.foundation.common_database.util.QueryBuilder;
+import reyga.starter.foundation.common_database.util.SafeQueryBuilder;
 
-public class QueryProcessor {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-    /**
-     *  Class for QueryBuilder Processing
-     */
+@RequiredArgsConstructor
+public abstract class QueryProcessor extends BaseLogging {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public <T> List<T> fetch(QueryBuilder builder, RowMapper<T> rowMapper) {
+        String sql = builder.build();
+        log.info("Constructed query: " + sql);
+        List<Object> params = builder.getParameters();
+        return jdbcTemplate.query(sql, rowMapper, params.toArray());
+    }
+
+    public <T> List<T> fetch(SafeQueryBuilder builder, RowMapper<T> rowMapper) {
+        String sql = builder.build();
+        log.info("Constructed query: " + sql);
+        List<Object> params = builder.getParameters();
+        return jdbcTemplate.query(sql, rowMapper, params.toArray());
+    }
+
+    public <T> Optional<T> fetchOne(QueryBuilder builder, RowMapper<T> rowMapper) {
+        List<T> results = fetch(builder, rowMapper);
+        return results.isEmpty() ? Optional.empty() : Optional.of(results.get(0));
+    }
+
+    public <T> Optional<T> fetchOne(SafeQueryBuilder builder, RowMapper<T> rowMapper) {
+        List<T> results = fetch(builder, rowMapper);
+        return results.isEmpty() ? Optional.empty() : Optional.of(results.get(0));
+    }
+
+    public int execute(QueryBuilder builder) {
+        String sql = builder.build();
+        log.info("Constructed query: " + sql);
+        List<Object> params = builder.getParameters();
+        return jdbcTemplate.update(sql, params.toArray());
+    }
+
+    public int execute(SafeQueryBuilder builder) {
+        String sql = builder.build();
+        log.info("Constructed query: " + sql);
+        List<Object> params = builder.getParameters();
+        return jdbcTemplate.update(sql, params.toArray());
+    }
+
+    public int[] batchExecute(QueryBuilder builder, List<Object[]> batchParams) {
+        String sql = builder.build();
+        log.info("Constructed query: " + sql);
+        return jdbcTemplate.batchUpdate(sql, batchParams);
+    }
+
+    public int[] batchExecute(SafeQueryBuilder builder, List<Object[]> batchParams) {
+        String sql = builder.build();
+        log.info("Constructed query: " + sql);
+        return jdbcTemplate.batchUpdate(sql, batchParams);
+    }
+
+    public int[] batchExecute(List<QueryBuilder> builders) {
+        List<Integer> results = new ArrayList<>();
+        for (QueryBuilder builder : builders) {
+            String sql = builder.build();
+            log.info("Query ==> : " + sql);
+            results.add(jdbcTemplate.update(sql, builder.getParameters().toArray()));
+        }
+        return results.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] batchExecute(List<QueryBuilder> builders, List<SafeQueryBuilder> safeBuilders, boolean isSafeBuilder) {
+        List<Integer> results = new ArrayList<>();
+        if (isSafeBuilder) {
+            for (SafeQueryBuilder safeBuilder : safeBuilders) {
+                String sql = safeBuilder.build();
+                log.info("Query ==> : " + sql);
+                results.add(jdbcTemplate.update(sql, safeBuilder.getParameters().toArray()));
+            }
+        } else {
+            for (QueryBuilder builder : builders) {
+                String sql = builder.build();
+                log.info("Query ==> : " + sql);
+                results.add(jdbcTemplate.update(sql, builder.getParameters().toArray()));
+            }
+        }
+        return results.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] batchExecute(String sql, List<Object[]> batchParams) {
+        return jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                Object[] params = batchParams.get(i);
+                for (int j = 0; j < params.length; j++) {
+                    ps.setObject(j + 1, params[j]);
+                }
+            }
+
+            @Override
+            public int getBatchSize() {
+                return batchParams.size();
+            }
+        });
+    }
+
+    public <T> List<T> fetchRaw(String sql, RowMapper<T> rowMapper, Object... params) throws DataAccessException {
+        return jdbcTemplate.query(sql, rowMapper, params);
+    }
+
+    public int executeRaw(String sql, Object... params) throws DataAccessException {
+        return jdbcTemplate.update(sql, params);
+    }
 }
